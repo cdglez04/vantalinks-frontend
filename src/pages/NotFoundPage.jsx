14a4d994e8c18566289cc73e0a1a4d66411b12e3
Carlos Daniel <cdg7327@gmail.com>
2026-02-27 00:04:56 -0600

    Create NotFoundPage.jsx

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,16 @@
+import { FontSizeIcon } from '@radix-ui/react-icons'
+import '../App.css'
+
+function NotFoundPage() {
+    return(
+        <>
+            <div className='ab-main-container'>
+                <h1>Error 404</h1>
+                <h3>Page not found</h3>
+            </div>
+            
+        </>
+    )
+}
+
+export default NotFoundPage
